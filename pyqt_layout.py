8a0614308a3c6196c87ae7009ece4b7b57a278ff
unknown <jaewoniik94@gmail.com>
2023-03-21 16:35:15 +0900

    Prac : PyQt5 Layout

--- pyqt_layout.py
+++ pyqt_layout.py
@@ -1,4 +1,4 @@
-# 절대적 배치
+# 절대적 배치, 박스 레이아웃
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
 
@@ -48,3 +48,37 @@ if __name__ == '__main__':
 
 
 # ---------------------------------------------------------------------------------------------------------------------------
+
+
+# 그리드 레이아웃 : 위젯의 공간을 행 (row)과 열 (column)로 구분
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit)
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout() # QGridLayout 생성
+        self.setLayout(grid) # 어플리케이션 창의 레이아웃으로 설정
+
+        grid.addWidget(QLabel('Title:'), 0, 0)
+        grid.addWidget(QLabel('Author:'), 1, 0)
+        grid.addWidget(QLabel('Review:'), 2, 0)
+
+        grid.addWidget(QLineEdit(), 0, 1)
+        grid.addWidget(QLineEdit(), 1, 1)
+        grid.addWidget(QTextEdit(), 2, 1) # QTextEdit() : 여러 줄의 텍스트를 수정할 수 있는 위젯
+
+        self.setWindowTitle('QGridLayout')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
